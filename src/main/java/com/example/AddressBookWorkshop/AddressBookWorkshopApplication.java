2fa989a374ea7318b73c441afea25b129384d5ac
SparshSingh10 <sparsh262002@gmail.com>
2025-03-10 22:32:55 +0530

    swagger added

--- src/main/java/com/example/AddressBookWorkshop/AddressBookWorkshopApplication.java
+++ src/main/java/com/example/AddressBookWorkshop/AddressBookWorkshopApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
+@EnableCaching
 @SpringBootApplication
+
 
 public class AddressBookWorkshopApplication {
 
